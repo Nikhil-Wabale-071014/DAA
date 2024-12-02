1be4098487884aa918efa3c635fc587842151d81
Nikhil-Wabale-071014 <nikhilwabale13@gmail.com>
2024-12-02 14:08:25 +0530

    Update assignC3.cpp

--- assignC3.cpp
+++ assignC3.cpp
@@ -1,52 +1,72 @@
-/*Implement Check if it is possible to transform one
-string to another.
-Statement Given two strings s1 and s2 (all letters in uppercase).
-Check if it is possible to convert s1 to s2 by performing following
-operations.
-1. Make some lowercase letters uppercase.
-2. Delete all the lowercase letters.
-Input: s1 = daBcd s2 = ABC Output: yes
-Input: s1 = argaju s2 = RAJ Output: yes*/
-#include<iostream>
-#include<string>
-#include<algorithm>
-using namespace std;
-int check(string s1,string s2){
-    char a[10],b[10]    ;
-    int count=0;
-    for(int i=0;i<s1.length();i++){
-       a[i]=tolower(s1[i]);
+// Problem Statement no. 09
+// Check if it is posible to transform one string to another
+// Statement: Given 2 strings s1 and s2(uppercase).
+// Check if it is possible to convert s1 to s2 by performing following operations
+// 1. Make some lowercase letters uppercase
+// 2. Delete all the lowercase letters
 
-       
+// Input s1 = daBcd s2 = ABC
+// Output yes
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to check if s1 can be converted to s2 using rules
+bool check(string s1, string s2)
+{
+    // calculate length
+    int n = s1.length();
+    int m = s2.length();
+
+    bool dp[n + 1][m + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            dp[i][j] = false;
+        }
     }
-    
-    for(int i=0;i<s2.length();i++){
-         b[i]=tolower(s2[i]);
-    }
-    for(int i=0;i<s1.length();i++){
-        for(int j=0;j<s1.length();j++){
-            if(a[i]==b[j]){
-                s1.erase(s1.begin() + i); 
-                count++;
-                break;
+
+    // mark 1st position as true
+    dp[0][0] = true;
+
+    for (int i = 0; i < s1.length(); i++)
+    {
+        for (int j = 0; j <= s2.length(); j++)
+        {
+            if (dp[i][j])
+            {
+                // If uppercase in s1 are same as s2
+                if (j < s2.length() && (toupper(s1[i]) == s2[j]))
+                {
+                    dp[i + 1][j + 1] = true;
+                }
+
+                if (!isupper(s1[i]))
+                {
+                    dp[i + 1][j] = true;
+                }
             }
         }
     }
-    if(count==s2.length()){
-        cout<<"\nYES";
+    return (dp[n][m]);
+}
+
+// Driver code
+int main()
+{
+    string s1, s2;
+    cout<<"Enter any two strings to find if rules apply to them"<<endl;
+    cin>>s1;
+    cin>>s2;
+    if (check(s1, s2))
+    {
+        cout << "YES";
     }
-    else{
-        cout<<"\nNO";
+    else
+    {
+        cout << "NO";
     }
 
-}
-int main(){
-    string s1,s2;
-    char a[10],b[10];
-    int count=0;
-    cout<<"\nEnter string 1 : ";
-    cin>>s1;
-    cout<<"\nEnter string 2 : ";
-    cin>>s2;
-    check(s1,s2);
+    return 0;
 }
